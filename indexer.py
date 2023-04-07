76ff04930a871a3842d09cb5832affb01bf99b0b
Gabriel Rocha <gabreuvcr@gmail.com>
2023-04-07 18:33:03 -0300

    Gerando tokens com regex

--- indexer.py
+++ indexer.py
@@ -5,8 +5,7 @@ import argparse
 import json
 import nltk
 from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-from nltk.util import ngrams
+from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 
 MEGABYTE = 1024 * 1024
@@ -23,71 +22,37 @@ def merge_document_fields(document: dict) -> str:
     return merged_document
 
 
-def remove_dot_between_words(document: str) -> str:
-    new_document = ''
-    for i in range(len(document)):
-        if document[i] == '.':
-            if (
-                i > 0 
-                and i < len(document) - 1
-                and document[i - 1].isdigit()
-                and document[i + 1].isdigit()
-            ):
-                new_document += document[i]
-            else:
-                new_document += ' '
-        else:
-            new_document += document[i]
-    return new_document
-
-
-def clean_document(document: str) -> str:
-    document = document.casefold()
-    document = document.replace('-', ' ')
-    document = remove_dot_between_words(document)
-    document = re.sub('[^A-Za-z0-9. ]+', '', document)
-
-    return document
-
-
 def produce_tokens(document: str) -> list[str]:
-    # nltk.download('punkt')
-    # nltk.download("stopwords")
+    nltk.download('punkt', quiet=True)
+    nltk.download("stopwords", quiet=True)
+    document = document.lower()
+    document = re.sub(r'[^A-Za-z0-9\.\-\_ ]+', '', document)
     stemmer = PorterStemmer()
     stop_words = set(stopwords.words('english'))
-    tokens = word_tokenize(document)
-
-    # twograms =  list(ngrams(tokens, 2))
-    # tokens += [' '.join(twogram) for twogram in twograms]
-    
+    tokenizer = RegexpTokenizer(r'\d*\.\d+|[a-zA-Z0-9]+')
+    tokens = tokenizer.tokenize(document)
     tokens = [w for w in tokens if not w in stop_words and len(w) >= 2]
     tokens = [stemmer.stem(w) for w in tokens]
-
     return tokens
 
 
 def produce_token_frequency(raw_tokens: list[str]) -> list[tuple[str, int]]:
     raw_tokens = sorted(raw_tokens)
-    
-    tokens = []
-    i, j = 0, 0
+    i, j, tokens = 0, 0, []
     while i < len(raw_tokens):
         if raw_tokens[i] != raw_tokens[j]:
             tokens += [(raw_tokens[j], i - j)]
             j = i
         i += 1
     tokens += [(raw_tokens[j], i - j)]
-
     return tokens
 
 
 def tokenize(document: dict) -> list[tuple[str, int]]:
     print(document, end="\n\n")
     merged_document = merge_document_fields(document)
-    cleared_document = clean_document(merged_document)
-    raw_tokens = produce_tokens(cleared_document) 
+    raw_tokens = produce_tokens(merged_document) 
     tokens = produce_token_frequency(raw_tokens)
-
     return tokens
    
 
@@ -107,7 +72,7 @@ def main(corpus_path: str, index_dir_path: str) -> None:
     with open(corpus_path, 'r') as f:
         corpus = [json.loads(line) for line in f]
     
-    inverted_index = index(corpus[:1])
+    inverted_index = index(corpus[:2])
     print(inverted_index)
 
 
