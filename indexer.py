5026d07c9dbf142c3b368193be36e08fa40b4da4
Gabriel Rocha <gabreuvcr@gmail.com>
2023-04-07 01:37:35 -0300

    Refatorando tokenize: separando em funcoes

--- indexer.py
+++ indexer.py
@@ -6,58 +6,95 @@ import json
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+from nltk.util import ngrams
 from nltk.corpus import stopwords
 
 MEGABYTE = 1024 * 1024
+
 def memory_limit(value: int):
     limit = value * MEGABYTE
     resource.setrlimit(resource.RLIMIT_AS, (limit, limit))
 
-def tokenize(text: str) -> list[tuple[str, int]]:
-    def remove_dot_between_words(text: str) -> str:
-        new_text = ''
-        for i in range(len(text)):
-            if text[i] == '.':
-                if (
-                    i > 0 
-                    and i < len(text) - 1
-                    and text[i - 1].isdigit()
-                    and text[i + 1].isdigit()
-                ):
-                    new_text += text[i]
-                else:
-                    new_text += ' '
-            else:
-                new_text += text[i]
-        return new_text
 
+def merge_document_fields(document: dict) -> str:
+    merged_document = document['title'] + ' ' + document['text']
+    for keyword in document['keywords']:
+        merged_document += ' ' + keyword
+    return merged_document
+
+
+def remove_dot_between_words(document: str) -> str:
+    new_document = ''
+    for i in range(len(document)):
+        if document[i] == '.':
+            if (
+                i > 0 
+                and i < len(document) - 1
+                and document[i - 1].isdigit()
+                and document[i + 1].isdigit()
+            ):
+                new_document += document[i]
+            else:
+                new_document += ' '
+        else:
+            new_document += document[i]
+    return new_document
+
+
+def clean_document(document: str) -> str:
+    document = document.casefold()
+    document = document.replace('-', ' ')
+    document = remove_dot_between_words(document)
+    document = re.sub('[^A-Za-z0-9. ]+', '', document)
+
+    return document
+
+
+def produce_tokens(document: str) -> list[str]:
     # nltk.download('punkt')
     # nltk.download("stopwords")
     stemmer = PorterStemmer()
     stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(document)
 
-    print(text, end="\n\n")
-    text = text.casefold()
-    text = text.replace('-', ' ')
-    text = remove_dot_between_words(text)
-    text = re.sub('[^A-Za-z0-9. ]+', '', text)
-
-    raw_tokens = word_tokenize(text)
-    raw_tokens = [w for w in raw_tokens if not w in stop_words and len(w) >= 2]
-    raw_tokens = [stemmer.stem(w) for w in raw_tokens]
+    # twograms =  list(ngrams(tokens, 2))
+    # tokens += [' '.join(twogram) for twogram in twograms]
     
-    tokens = []
-    uniques_words = set(raw_tokens)
-    for word in uniques_words:
-        tokens += [(word, raw_tokens.count(word))]
+    tokens = [w for w in tokens if not w in stop_words and len(w) >= 2]
+    tokens = [stemmer.stem(w) for w in tokens]
 
     return tokens
+
+
+def produce_token_frequency(raw_tokens: list[str]) -> list[tuple[str, int]]:
+    raw_tokens = sorted(raw_tokens)
+    
+    tokens = []
+    i, j = 0, 0
+    while i < len(raw_tokens):
+        if raw_tokens[i] != raw_tokens[j]:
+            tokens += [(raw_tokens[j], i - j)]
+            j = i
+        i += 1
+    tokens += [(raw_tokens[j], i - j)]
+
+    return tokens
+
+
+def tokenize(document: str) -> list[tuple[str, int]]:
+    print(document, end="\n\n")
+    merged_document = merge_document_fields(document)
+    cleared_document = clean_document(merged_document)
+    raw_tokens = produce_tokens(cleared_document) 
+    tokens = produce_token_frequency(raw_tokens)
+
+    return tokens
+   
 
 def index(corpus: str) -> dict[str, list[tuple[int, int]]]:
     inverted_index = {}
     d_id = 0
     for document in corpus:
-        document = merge_corpus_fields(document)
         d_id += 1
         for (term, freq) in tokenize(document):
             if term not in inverted_index:
@@ -65,22 +102,17 @@ def index(corpus: str) -> dict[str, list[tuple[int, int]]]:
             inverted_index[term] += [(d_id, freq)]
     return inverted_index
 
-def merge_corpus_fields(line: dict) -> str:
-    merged_text = line['title'] + ' ' + line['text']
-    for keyword in line['keywords']:
-        merged_text += ' ' + keyword
-    return merged_text
 
 def main(corpus_path: str, index_dir_path: str) -> None:
     with open(corpus_path, 'r') as f:
         corpus = [json.loads(line) for line in f]
     
-    # inverted_index = index(corpus)
-    # inverted_index = index(corpus[8:9])
-    inverted_index = index(corpus[:18])
-    # inverted_index = index(corpus[:4])
-    print()
-    print(dict(sorted(inverted_index.items(), key=lambda item: item[0])))
+    inverted_index = index(corpus[:1])
+    inverted_index = dict(
+        sorted(inverted_index.items(), 
+        key=lambda item: item[0])
+    )
+    print(inverted_index)
 
 
 if __name__ == "__main__":
